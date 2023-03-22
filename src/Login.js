aa5b7b9b39cd93ba84534b2e013f9f06a811f03b
oluwapelumi1 <oluwapelumidaniel88@gmail.com>
2023-03-22 13:40:39 +0100

    minor fix

---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+    return (
+        <div className='login'>
+            {/* Spotify logo */}
+            {/* Login with Spotify */}
+            <h1>am the login page</h1>
+        </div>
+    )
+}
+
+export default Login
